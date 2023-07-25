1b05bd596803682a9d43ba7d0ba01062cae2e90c
Marina Konovalchik <marina200014@yandex.by>
2023-07-26 00:31:25 +0300

    added Tests to public class name

--- src/test/java/in/reqres/ReqresInTests.java
+++ src/test/java/in/reqres/ReqresInTests.java
@@ -6,7 +6,7 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
-public class ReqresIn extends TestBase {
+public class ReqresInTests extends TestBase {
 
     @Test
     void createUserTest() {
